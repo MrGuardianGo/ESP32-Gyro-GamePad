230d62a21255fa8ab1671004879b3583239112e2
MrGuardianGo <dr.saidongargaonka@gmail.com>
2026-02-27 19:04:18 +0530

    final-version

--- bluetooth_gyro_controller.ino
+++ bluetooth_gyro_controller.ino
@@ -3,33 +3,10 @@
 #include "Wire.h"  // This library allows you to communicate with I2C devices.
 #include <BleGamepad.h>
 
-#define numOfButtons 13
-#define numOfHatSwitches 0
-#define enableX true
-#define enableY true
-#define enableZ true
-#define enableRX true
-#define enableRY true
-#define enableRZ true
-#define enableSlider1 true
-#define enableSlider2 true
-#define enableRudder false
-#define enableThrottle false
-#define enableAccelerator false
-#define enableBrake false
-#define enableSteering false
-
-#define R2_BUTTON 32
-#define L2_BUTTON 35
-
-#define NUM_OF_BUTTONS 13
-
-
 BleGamepad bleGamepad("ESP32 BLE Gamepad", "One Man Hit Parade", 100);
 
-int16_t simMin = 0x8000;
-int16_t axesCenter = 0x00;
-int16_t simMax = 0x7FFF;
+float smoothed_accel_y = 0;
+const float smoothingFactor = 0.3;
 
 int IDLE_LOWEST = -2300;
 int IDLE_HIGHEST = 1200;
@@ -47,14 +24,18 @@ int TOTAL_RIGHT_LOWEST = 5500;
 const int buttonW = 32;
 const int buttonS = 35;
 
+int xAxis = 32736 / 2;
+
 #define SDA_PIN 26
 #define SCL_PIN 25
 
-int DEADZONE_LOW = -2300;
-int DEADZONE_HIGH = 1200;
+unsigned long accelStartTime = 0;
+bool accelRamping = false;
 
-int MAX_LEFT = -6000;
-int MAX_RIGHT = 5500;
+unsigned long brakeStartTime = 0;
+bool brakeRamping = false;
+
+const unsigned long rampDuration = 1000;
 
 const int MPU_ADDR = 0x68;
 
@@ -62,9 +43,9 @@ int16_t accelerometer_x, accelerometer_y, accelerometer_z;
 int16_t gyro_x, gyro_y, gyro_z;
 int16_t temperature;
 
-char tmp_str[7];  // temporary variable used in convert function
+char tmp_str[7]; 
 
-char* convert_int16_to_str(int16_t i) {  // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+char* convert_int16_to_str(int16_t i) { 
   sprintf(tmp_str, "%6d", i);
   return tmp_str;
 }
@@ -79,80 +60,53 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.println("Starting BLE...");
-  BleGamepadConfiguration bleGamepadConfig;
-  bleGamepadConfig.setAutoReport(false);
-  bleGamepadConfig.setControllerType(CONTROLLER_TYPE_GAMEPAD);
-  bleGamepadConfig.setButtonCount(numOfButtons);
-  bleGamepadConfig.setWhichAxes(enableX, enableY, enableZ, enableRX, enableRY, enableRZ, enableSlider1, enableSlider2);
-  bleGamepadConfig.setWhichSimulationControls(enableRudder, enableThrottle, enableAccelerator, enableBrake, enableSteering);
-  bleGamepadConfig.setHatSwitchCount(numOfHatSwitches);
-  bleGamepadConfig.setSimulationMin(simMin);
-  bleGamepadConfig.setSimulationMax(simMax);
 
-  bleGamepad.begin(&bleGamepadConfig);
-
-  bleGamepad.setBrake(simMin);
-  bleGamepad.setAccelerator(simMax);
+  bleGamepad.begin();
 
   Serial.println("BLE Started");
 }
 
 void loop() {
-  Wire.beginTransmission(MPU_ADDR);
-  Wire.write(0x3B);
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU_ADDR, 7 * 2, true);
-
-  accelerometer_x = Wire.read() << 8 | Wire.read();
-  accelerometer_y = Wire.read() << 8 | Wire.read();
-
   if (bleGamepad.isConnected()) {
-    int xAxisValue = 0;
-    if (accelerometer_y > DEADZONE_LOW && accelerometer_y < DEADZONE_HIGH) {
-      xAxisValue = 0;
-    } else {
-      xAxisValue = map(accelerometer_y, MAX_LEFT, MAX_RIGHT, -32767, 32767);
+    Wire.beginTransmission(MPU_ADDR);
+    Wire.write(0x3B);
+    Wire.endTransmission(false);
+    Wire.requestFrom(MPU_ADDR, 7 * 2, true);
 
-      xAxisValue = constrain(xAxisValue, -32767, 32767);
+    accelerometer_x = Wire.read() << 8 | Wire.read();
+    accelerometer_y = Wire.read() << 8 | Wire.read();
+
+    smoothed_accel_y = smoothingFactor * accelerometer_y + (1 - smoothingFactor) * smoothed_accel_y;
+
+    int filtered_y = (int)smoothed_accel_y;
+
+    int tilt = 0;
+    if (filtered_y > IDLE_LOWEST && filtered_y < IDLE_HIGHEST) {
+      tilt = 32736 / 2;
+    } else if (filtered_y >= SLIGHT_RIGHT_LOWEST && filtered_y <= SLIGHT_RIGHT_HIGHEST) {
+      tilt = map(filtered_y, SLIGHT_RIGHT_LOWEST, SLIGHT_RIGHT_HIGHEST, 32736 / 2, 32736);
+    } else if (filtered_y > TOTAL_RIGHT_LOWEST) {
+      tilt = 32737;
+    } else if (filtered_y >= SLIGHT_LEFT_LOWEST && filtered_y <= SLIGHT_LEFT_HIGHEST) {
+      tilt = 32736 / 2 - map(filtered_y, SLIGHT_LEFT_LOWEST, SLIGHT_LEFT_HIGHEST, 32736 / 2, 0);
+    } else if (filtered_y <= TOTAL_LEFT_HIGHEST) {
+      tilt = 0;
     }
 
-    int rightTrigger = 0;
-    int leftTrigger = 0;
-
-    Serial.println(xAxisValue);
+    xAxis = tilt;
 
     if (digitalRead(buttonW)) {
       bleGamepad.press(8);
-      Serial.println("Accer");
+    } else {
+      bleGamepad.release(8);
     }
 
     if (digitalRead(buttonS)) {
       bleGamepad.press(7);
-      Serial.println("Brake");
+    } else {
+      bleGamepad.release(7);
     }
 
-    bleGamepad.setX(xAxisValue);
-    bleGamepad.setY(0);
-    bleGamepad.setZ(0);
-    bleGamepad.setRX(0);
-
-    delay(10);
+    bleGamepad.setAxes(xAxis, 32736 / 2, 32736 / 2, 32736 / 2, 32736 / 2, 32736 / 2, 0, 0);
   }
-
-  // if (accelerometer_y > IDLE_LOWEST && accelerometer_y < IDLE_HIGHEST) {
-  //   tiltR = 0;
-  //   tiltL = 0;
-  // } else if (accelerometer_y >= SLIGHT_RIGHT_LOWEST && accelerometer_y <= SLIGHT_RIGHT_HIGHEST) {
-  //   tiltR = map(accelerometer_y, SLIGHT_RIGHT_LOWEST, SLIGHT_RIGHT_HIGHEST, 0, 32736);
-  //   tiltL = 0;
-  // } else if (accelerometer_y > TOTAL_RIGHT_LOWEST) {
-  //   tiltR = 32737;
-  //   tiltL = 0;
-  // } else if (accelerometer_y >= SLIGHT_LEFT_LOWEST && accelerometer_y <= SLIGHT_LEFT_HIGHEST) {
-  //   tiltR = 0;
-  //   tiltL = map(accelerometer_y, SLIGHT_LEFT_LOWEST, SLIGHT_LEFT_HIGHEST, 0, 32736);
-  // } else if (accelerometer_y <= TOTAL_LEFT_HIGHEST) {
-  //   tiltR = 0;
-  //   tiltL = 32737;
-  // }
 }
